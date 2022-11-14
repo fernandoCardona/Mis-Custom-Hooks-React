f986a13f9adc6aed0cb863eb189b352c00377993
Fernando Cardona <fernando.cardona@the-lion.com>
2022-11-14 16:53:21 +0100

    firebase firebase-elements-in-coleccion

---
+++ Helpers/firebase/firebase-elements-in-coleccion.js
@@ -0,0 +1,25 @@
+//1.-Extraer todos los elementos de una coleccion: (habiendo instalado el "npm install firebase")
+import firebase from 'firebase';
+
+
+export const mostrarDocumentos = ( snapshot: firebase.forestore.QuerySnapshot ) => {
+    const documentos: any[] = [];
+			snapshot.forEach( snapHijo => {
+				documentos.push( {
+					id: snapHijo.id,
+					...snapHijo.data()
+				});
+            });
+    console.log(documentos);
+    return documentos;
+}
+
+//2.-en el archivo llamamos al helper 
+import mostrarDocumentos from '../helper/firebase-elements-in-coleccion'
+
+coleccionRef
+    .onsnapshot( snap =>{
+        mostrarDocumentos( snap );
+    })
+    //Version corta 
+    //coleccionRef( mostrarDocumentos);
