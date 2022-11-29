3659d8b96b9c84a46c7b8394bb5ed9772a66baf8
fernandoCardona <48406680+fernandoCardona@users.noreply.github.com>
2022-11-29 13:48:05 +0100

    Update useFetch.js

--- Hooks/useFetch/useFetch.js
+++ Hooks/useFetch/useFetch.js
@@ -1,36 +1,42 @@
-import { useEffect, useState, useRef } from 'react';
- ;
+import { useEffect, useState } from "react";
+
 
 export const useFetch = ( url ) => {
 
-    const isMounted = useRef(true);
-    const [state, setState] = useState( { data: null, loading: true, error: null } );
-    useState(() => {
-        return () =>{
-            isMounted.current = false;
-        }
-    }, []);
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null,
+    })
+
+
+    const getFetch = async () => {
+
+        setState({
+            ...state,
+            isLoading: true,
+        });
+
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        setState({
+            data,
+            isLoading: false,
+            hasError: null,
+        });
+    }
+
+
+    useEffect(() => {
+        getFetch();
+    }, [url])
     
-    useEffect(() => { 
 
-        setState( { data: null, loading:true, error: null } );
-        
-        fetch( url )
-            .then( resp => resp.json() )
-            .then( data => {
-               
-                    if ( isMounted.current ) {
-                        setState({ 
-                            loading: false,
-                            error: null, 
-                            data
-                        }); 
-                    };
 
-            });
-
-    },[ url ]);
-
-    return state;
-
+    return {
+        data:      state.data,
+        isLoading: state.isLoading,
+        hasError:  state.hasError,
+    };
 }
