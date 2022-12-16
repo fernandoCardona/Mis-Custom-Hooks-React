39740aeec54d7d791e414f97ffd19a424c16e7f7
fernandoCardona <48406680+fernandoCardona@users.noreply.github.com>
2022-12-16 12:00:00 +0100

    Update useForm.js

--- Hooks/useForm/useForm.js
+++ Hooks/useForm/useForm.js
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {}) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [ formValidation, setFormValidation ] = useState({});
+
+    useEffect(() => {
+        createValidators();
+    }, [ formState ])
+
+    useEffect(() => {
+        setFormState( initialForm );
+    }, [ initialForm ])
+    
+    
+    const isFormValid = useMemo( () => {
+
+        for (const formValue of Object.keys( formValidation )) {
+            if ( formValidation[formValue] !== null ) return false;
+        }
+
+        return true;
+    }, [ formValidation ])
+
 
     const handleInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,13 +36,30 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () => {
+        
+        const formCheckedValues = {};
+        
+        for (const formField of Object.keys( formValidations )) {
+            const [ fn, errorMessage ] = formValidations[formField];
+
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+        }
+
+        setFormValidation( formCheckedValues );
+    }
+
+
+
     return {
         ...formState,
         formState,
         handleInputChange,
         handleResetForm,
+
+        ...formValidation,
+        isFormValid
     }
 }
-
 
  
